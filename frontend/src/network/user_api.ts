de5f7bf3c8862f57614760a52ba7f23ed45665b3
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2024-09-02 22:23:57 -0500

    fixed

--- frontend/src/network/user_api.ts
+++ frontend/src/network/user_api.ts
@@ -1,5 +1,6 @@
 import { User } from "../models/user";
 
+import { UnauthorizedError } from "../network/http-errors";
 export interface SignUpCredentials {
   name: string;
   email: string;
@@ -29,16 +30,24 @@ export async function Login(credentials: LoginCredentials): Promise<User> {
     },
     body: JSON.stringify(credentials),
   });
+  if (!response.ok) {
+    if (response.status === 401) {
+      throw new UnauthorizedError("Invalid credentials");
+    }
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
   return response.json();
 }
 
 export async function getAuthenticatedUser() {
-  const response = await fetch("http://localhost:8080/api/users/authenticated-user", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const response = await fetch(
+    "http://localhost:8080/api/users/authenticated-user",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
   return response.json();
-  
 }
